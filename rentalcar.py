e2d924fc39fe3843e22683e5146a95eb8e8d3a8e
Calkins Kurtis <kurtiscalkins@yahoo.com>
2022-06-10 14:17:27 -0600

    Added some comments to explain each function

--- rentalcar.py
+++ rentalcar.py
@@ -2,19 +2,21 @@
 from cars import *
 from linkedList import *
 
-
+# Runs a loop through the list of car types to add each one to a linked list
 def insert_car_types():
     car_type_list = LinkedList()
     for type in car_type:
         car_type_list.insert_beginning(type)
     return car_type_list
 
+# Runs a loop through the list of cars to add each one to a linked list
 def insert_cars():
     car_list = LinkedList()
     for car in cars:
         car_list.insert_beginning(car)
     return car_list
 
+# Traverses the cars linked list to filter the cars based on the car type
 def find_cars(car_type, linked_list):
     current_node = linked_list.get_head_node()
     present_list = []
@@ -24,6 +26,7 @@ def find_cars(car_type, linked_list):
         current_node = current_node.get_next_node()
     return present_list
 
+# Transforms the list of cars into a more presentable and readable format
 def make_cars_readable(ls):  
     for i in ls:
         print("-"*20 + "\n")
@@ -35,6 +38,7 @@ def make_cars_readable(ls):
         print("\n\n")
     return ""
 
+# Traverses the car type list to filter them based on a string (i.e. finds the car types from a user inputting the first letters)
 def find_car_type(string, linked_list):
     current_node = linked_list.get_head_node()
     type_list = []
@@ -44,6 +48,7 @@ def find_car_type(string, linked_list):
         current_node = current_node.get_next_node()
     return type_list
 
+# Transforms the list of car types into a more presentable and readable format
 def make_types_readable(ls):
     for i in ls:
         print("- " + i )
